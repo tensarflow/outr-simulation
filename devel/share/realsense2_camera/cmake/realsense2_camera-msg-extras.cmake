03560e65dc815dd82ce842bfa1a805cdaad6e6f3
Tayyip Ensar Özkaya <te.ozkaya@gmail.com>
2020-05-05 17:46:04 +0300

    added laserscanner from pointcloud functionality, added new package for navigation stack, readme to be updated

---
+++ devel/share/realsense2_camera/cmake/realsense2_camera-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(realsense2_camera_MESSAGE_FILES "/home/ensar/outr-simulation/src/realsense-ros/realsense2_camera/msg/IMUInfo.msg;/home/ensar/outr-simulation/src/realsense-ros/realsense2_camera/msg/Extrinsics.msg")
+set(realsense2_camera_SERVICE_FILES "")
